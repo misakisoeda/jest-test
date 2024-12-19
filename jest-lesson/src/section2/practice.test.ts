1ecb3190f8b5bba07f3fa50722181a5b4558b3cb
misakisoeda <misaki.soeda.29@gmail.com>
2024-12-19 22:22:42 +0900

    fix

--- jest-lesson/src/section2/practice.test.ts
+++ jest-lesson/src/section2/practice.test.ts
@@ -9,38 +9,34 @@ describe("演習問題", () => {
 
     describe("addItem", () => {
         it("1. `addItem`メソッドが、アイテムをリストに追加できることを確認するテストケース", () => {
-            shoppingList.addItem("追加テキスト");
+            shoppingList.addItem("メロン");
             expect(shoppingList.list).toHaveLength(1);  // 配列や文字列のサイズを確認
-            expect(shoppingList.list[0]).toBe("追加テキスト");  // 0番目の文字は「ほげ」か確認
+            expect(shoppingList.list[0]).toBe("メロン");  // 0番目の文字は「メロン」か確認
         })
 
-        it("既存のリストに新しい項目を追加できる", () => {  // 追加
-            shoppingList.addItem("ほげ");
-            shoppingList.addItem("ふが");
+        it("リストに複数アイテムを追加できる", () => {  // 追加
+            shoppingList.addItem("リンゴ");
+            shoppingList.addItem("ミカン");
             expect(shoppingList.list).toHaveLength(2);  // 配列や文字列のサイズを確認
-            expect(shoppingList.list).toEqual(["ほげ", "ふが"]);    // objectを比較するのでtoEqual
+            expect(shoppingList.list).toEqual(["リンゴ", "ミカン"]);    // objectを比較するのでtoEqual
         });
     });
 
     describe("removeItem", () => {
         it("2. `removeItem`メソッドが、アイテムをリストから削除できることを確認するテストケース", () => {
-            shoppingList.addItem("追加テキスト");
-            shoppingList.addItem("ほげ");
-            shoppingList.removeItem("ほげ");
+            shoppingList.addItem("メロン");
+            shoppingList.addItem("リンゴ");
+            shoppingList.removeItem("リンゴ");
+            expect(shoppingList.list).not.toContain("リンゴ");  // アイテムが配列内にないことを確認
             expect(shoppingList.list).toHaveLength(1);  // 配列や文字列のサイズを確認
-            expect(shoppingList.list[0]).toBe("追加テキスト");  // 0番目の文字は「ほげ」か確認
+            expect(shoppingList.list[0]).toBe("メロン");  // 0番目の文字は「メロン」か確認
         })
 
         it("3. `removeItem`メソッドが、存在しないアイテムの削除を試みたときにエラーをスローすることを確認するテストケース", () => {
-            shoppingList.addItem("追加テキスト");
-
             // エラーがスローされることを確認
             expect(() => {
-                shoppingList.removeItem("ほげ");
-            }).toThrow("アイテム: ほげ は存在しません");
-
-            expect(shoppingList.list).toHaveLength(1);  // 配列や文字列のサイズを確認
-            expect(shoppingList.list[0]).toBe("追加テキスト");  // 0番目の文字は「ほげ」か確認
+                shoppingList.removeItem("リンゴ");
+            }).toThrow("アイテム: リンゴ は存在しません");
         })
     });
 })
